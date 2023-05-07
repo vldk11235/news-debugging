2c834d4f7298b4fb36ed01b55e45f1dd4d5a78a4
Vlad Kremenchuk <vlad.kremenchuk@gmail.com>
2023-05-07 17:28:19 +0300

    pagination and error checking added

--- src/components/NewsListByFilters.js
+++ src/components/NewsListByFilters.js
@@ -54,23 +54,29 @@ import axios from 'axios';
 import { useLocation } from 'react-router-dom';
 import NewsItem from './NewsItem';
 import Sidebar from './Sidebar';
+import {Box, Container, CssBaseline, Pagination} from "@mui/material";
+
+
 
 const NewsListByFilters = () => {
     const [articles, setArticles] = useState([]);
     const [filters, setFilters] = useState({
-        category: '',
-        country: '',
+        // category: '',
+        // country: '',
         searchTerm: '',
     });
+    const [currentPage, setCurrentPage] = useState(1);
+    const [itemsPerPage] = useState(10);
 
-    const location = useLocation();
-    const initialCategory = new URLSearchParams(location.search).get('category');
 
-    useEffect(() => {
-        if (initialCategory) {
-            setFilters((prevFilters) => ({ ...prevFilters, category: initialCategory }));
-        }
-    }, [initialCategory]);
+    // const location = useLocation();
+    // const initialCategory = new URLSearchParams(location.search).get('category');
+
+    // useEffect(() => {
+    //     if (initialCategory) {
+    //         setFilters((prevFilters) => ({ ...prevFilters, category: initialCategory }));
+    //     }
+    // }, [initialCategory]);
 
     // useEffect(() => {
     //     const { category, country, searchTerm } = filters;
@@ -95,15 +101,15 @@ const NewsListByFilters = () => {
     // }, [filters]);
 
     useEffect(() => {
-        const { category, country, searchTerm } = filters;
-        const apiUrl = 'https://newsapi.org/v2/top-headlines';
+        const { searchTerm } = filters;
+        const apiUrl = 'https://newsapi.org/v2/everything';
         const apiKey = process.env.REACT_APP_API_KEY;
         console.log(searchTerm);
 
         let query = `?pageSize=50&language=en&apiKey=${apiKey}`;
 
-        if (category) query += `&category=${category}`;
-        if (country) query += `&country=${country}`;
+        // if (category) query += `&category=${category}`;
+        // if (country) query += `&country=${country}`;
         if (searchTerm) query += `&q=${searchTerm}`;
 
         axios
@@ -124,11 +130,54 @@ const NewsListByFilters = () => {
         }));
     };
 
+    const indexOfLastItem = currentPage * itemsPerPage;
+    const indexOfFirstItem = indexOfLastItem - itemsPerPage;
+    const currentItems = articles.slice(indexOfFirstItem, indexOfLastItem);
+
+    const pageCount = Math.ceil(articles.length / itemsPerPage);
+
+    const handleChange = (event, value) => {
+        setCurrentPage(value);
+    };
+
+
     console.log(articles);
     // console.log(searchTerm);
 
-    return (
-        <div className="news-app">
+    return ( <React.Fragment>
+        <CssBaseline/>
+            <Sidebar onFilterChange={handleFilterChange} />
+        <Container maxWidth="md">
+            <Box
+                sx={{
+                    // height: "100vh",
+                    width: "100%",
+                    mt: 5,
+                    display: "flex",
+                    flexWrap: "wrap",
+                    justifyContent: "space-around",
+                }}
+            >
+                {currentItems.map((article, index) => (
+                    <NewsItem
+                        key={index}
+                        title={article.title}
+                        description={article.description}
+                        urlToImage={article.urlToImage}
+                        url={article.url}
+                    />
+                ))}
+            </Box>
+            <Pagination
+                count={pageCount}
+                page={currentPage}
+                onChange={handleChange}
+                color="primary"
+                sx={{mt: 3, mb: 5, display: "flex", justifyContent: "center"}}
+            />
+        </Container>
+    </React.Fragment>
+        /*<div className="news-app">
             <Sidebar onFilterChange={handleFilterChange} />
             <div className="news-list">
                 {articles.map((article, index) => (
@@ -141,7 +190,7 @@ const NewsListByFilters = () => {
                     />
                 ))}
             </div>
-        </div>
+        </div>*/
     );
 };
 
--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -6,13 +6,13 @@ const countries = ['us', 'ua', 'au', 'ca', 'in', 'fr', 'de', 'jp'];
 const Sidebar = ({onFilterChange}) => {
     const [searchTerm, setSearchTerm] = useState('');
 
-    const handleCategoryChange = (e) => {
-        onFilterChange({filterType: 'category', value: e.target.value});
-    };
+    // const handleCategoryChange = (e) => {
+    //     onFilterChange({filterType: 'category', value: e.target.value});
+    // };
 
-    const handleCountryChange = (e) => {
-        onFilterChange({filterType: 'country', value: e.target.value});
-    };
+    // const handleCountryChange = (e) => {
+    //     onFilterChange({filterType: 'country', value: e.target.value});
+    // };
 
     const handleSearchInputChange = (e) => {
         setSearchTerm(e.target.value);
@@ -35,28 +35,28 @@ const Sidebar = ({onFilterChange}) => {
                 </label>
                 <button type="submit">Search</button>
             </form>
-            <label>
-                Category:
-                <select onChange={handleCategoryChange}>
-                    <option value="">All</option>
-                    {categories.map((category, index) => (
-                        <option key={index} value={category}>
-                            {category}
-                        </option>
-                    ))}
-                </select>
-            </label>
-            <label>
-                Country:
-                <select onChange={handleCountryChange}>
-                    <option value="">All</option>
-                    {countries.map((country, index) => (
-                        <option key={index} value={country}>
-                            {country.toUpperCase()}
-                        </option>
-                    ))}
-                </select>
-            </label>
+            {/*<label>*/}
+            {/*    Category:*/}
+            {/*    <select onChange={handleCategoryChange}>*/}
+            {/*        <option value="">All</option>*/}
+            {/*        {categories.map((category, index) => (*/}
+            {/*            <option key={index} value={category}>*/}
+            {/*                {category}*/}
+            {/*            </option>*/}
+            {/*        ))}*/}
+            {/*    </select>*/}
+            {/*</label>*/}
+            {/*<label>*/}
+            {/*    Country:*/}
+            {/*    <select onChange={handleCountryChange}>*/}
+            {/*        <option value="">All</option>*/}
+            {/*        {countries.map((country, index) => (*/}
+            {/*            <option key={index} value={country}>*/}
+            {/*                {country.toUpperCase()}*/}
+            {/*            </option>*/}
+            {/*        ))}*/}
+            {/*    </select>*/}
+            {/*</label>*/}
         </div>
     );
 };
